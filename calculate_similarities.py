931b3b5b55ca92bfa795bc3d45f7d7e57aa4d30c
John Lalor <john.lalor@nd.edu>
2025-03-03 08:25:40 -0500

    update

--- calculate_similarities.py
+++ calculate_similarities.py
@@ -12,18 +12,35 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-m', "--model",)    
-parser.add_argument('-d', "--data",)    
+#parser.add_argument('-m', "--model",)    
+#parser.add_argument('-d', "--data",)    
 
+parser.add_argument('-t', "--task",)    
 args = parser.parse_args()
+
+
+taskID = int(args.task)
+
+Models=["skip", "all-MiniLM-L6-v2", "all-MiniLM-L6-v2", "all-mpnet-base-v2", "all-mpnet-base-v2", "bert_CLS", "bert_CLS"]
+
+LLMs=["skip", "4o_newnoprompt", "llama_newnoprompt", "4o_newnoprompt", "llama_newnoprompt", "4o_newnoprompt", "llama_newnoprompt"]
 
 #sbert_model = "all-MiniLM-L6-v2"
 #dataInput = "4o_all"
 
-sbert_model = args.model
-dataInput = args.data
+#sbert_model = args.model
+#dataInput = args.data
 
-model = SentenceTransformer(sbert_model)
+sbert_model = Models[taskID]
+dataInput = LLMs[taskID]
+
+if sbert_model == "bert_CLS":
+    bert_version = 'bert-base-uncased'
+    #'mnaylor/psychbert-finetuned-mentalhealth'
+    tokenizer = BertTokenizer.from_pretrained(bert_version)
+    model = BertModel.from_pretrained(bert_version)
+else:
+    model = SentenceTransformer(sbert_model)
 #model = SentenceTransformer("all-mpnet-base-v2")
 
 #results_df = pd.read_csv(f"~/data/psychagents/experiment_results_{dataInput}.csv")
@@ -32,10 +49,6 @@ results_df = pd.read_csv(f"./Data/experiment_results_{dataInput}.csv")
 # Load BERT model and tokenizer
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-#bert_version = 'bert-base-uncased'
-#'mnaylor/psychbert-finetuned-mentalhealth'
-#tokenizer = BertTokenizer.from_pretrained(bert_version)
-#model = BertModel.from_pretrained(bert_version)
 model = model.eval()
 model = model.to(device)
 
@@ -55,6 +68,9 @@ def calculate_similarity_bert(results_df):
             generated_response = row[f"{question} Generated"]
             true_label = row[f"{question} True"]
 
+            if generated_response == "skip":
+                continue
+
             documents = [generated_response, true_label]
 
             #print(generated_response)
@@ -66,15 +82,16 @@ def calculate_similarity_bert(results_df):
             ]
 
             try:
-                embeddings = model.encode(sentences)
-                #print(embeddings.shape)
-                #generated_embedding = get_bert_embedding(generated_response)
-                #true_embedding = get_bert_embedding(true_label)
+                if sbert_model == "bert_CLS":
+                    generated_embedding = get_bert_embedding(generated_response)
+                    true_embedding = get_bert_embedding(true_label)
+                    similarity = 1 - cosine(generated_embedding, true_embedding)
+                else:
+                    embeddings = model.encode(sentences)
+                    similarity = model.similarity(embeddings, embeddings)
+                    similarity = similarity[0][1].detach().item()
             except:
                 continue
-            similarity = model.similarity(embeddings, embeddings)
-            #similarity = 1 - cosine(generated_embedding, true_embedding)
-            #similarity = similarities[0][1].detach().item()
             
             similarity_results.append({
                 'Sample Index': row['Sample Index'],
